ba7eb803c816518bfab3a3b839ffc8a8f7710bc6
Samuel Maddock <samuel-maddock@hotmail.com>
2023-02-24 10:46:09 +0000

    Fix empirical eps composition in example_plotter

--- plotting/example_plotter.py
+++ plotting/example_plotter.py
@@ -19,8 +19,8 @@ import seaborn as sns
 import torch
 
 
-sys.path.append("../../canife")
-sys.path.append("../../privacy_lint")
+sys.path.append("../canife")
+sys.path.append("../privacy_lint")
 
 from collections import defaultdict
 
@@ -104,49 +104,63 @@ def save_plot(name="", fig=None):
         plt.savefig(f"{BASE_PATH}/{name}.pdf", bbox_inches='tight', format="pdf")
     plt.clf()
 
-def extract_epsilon_metrics(df, override_empirical_eps=False):
+def extract_epsilon_metrics(df, override_empirical_eps=False, use_max_acc=False):
     extra_cols = defaultdict(list)
     if override_empirical_eps:
         print("Extracting empirical epsilon data...")
         analyser = CanaryAnalyser(None, None, None)
+        
         for idx, x in df.iterrows():
             with_dot_prods = ast.literal_eval(x["dot_prods_with_canary"].replace('nan,', ''))
             without_dot_prods = ast.literal_eval(x["dot_prods_without_canary"].replace('nan,', ''))
             results = AttackResults(torch.tensor(with_dot_prods), torch.tensor(without_dot_prods))
             
-            threshold = results.get_max_accuracy_threshold()[0]
-            # threshold = 0.5 
+            max_acc_thresh = results.get_max_accuracy_threshold()[0]
+            n_pos, n_neg = len(results.scores_train), len(results.scores_test)            
+            max_empirical_eps = 0
             
-            n_neg = len(results.scores_train)
-            n_pos = len(results.scores_test)
+            _, scores = results._get_scores_and_labels_ordered()
+            tpr_fpr = results.get_tpr_fpr()
             
-            tp = int((results.scores_train > threshold).sum().item())
-            fp = int((results.scores_test > threshold).sum().item())
-            tn = int((results.scores_test <= threshold).sum().item())
-            fn = int((results.scores_train <= threshold).sum().item())
-
-            fpr = fp/(fp+tn) 
-            fnr = fn/(fn+tp)
-            tpr = 1-fnr
-
-            empirical_eps = analyser.empirical_eps(tpr, fpr)
-            lower_eps = analyser.ci_eps(fp, fn, n_pos, n_neg)
-            upper_eps = analyser.ci_eps(fp, fn, bound="upper", n_pos=n_pos, n_neg=n_neg)
+            # delta = 1e-5
+            delta = 1/(n_pos + n_neg)
             
-            extra_cols["fp"].append(fp)
-            extra_cols["fn"].append(fn)
-            extra_cols["tp"].append(tp)
-            extra_cols["tn"].append(tn)
+            if use_max_acc: # Calculate empirical eps from max acc threshold
+                tp = int((results.scores_train >= max_acc_thresh).sum().item())
+                fp = int((results.scores_test >= max_acc_thresh).sum().item())
+                max_fp, max_fn, max_tp, max_tn = fp, n_pos-tp, tp, n_neg-fp
+                max_tpr, max_fpr = max_tp / (max_tp + max_fn), max_fp/(max_fp+max_tn)
+                max_empirical_eps = analyser.empirical_eps(max_tpr, max_fpr, delta=delta)
+            else: # Maximise empirical eps over TPR/FPR
+                for i, t in enumerate(scores):
+                    tpr, fpr = tpr_fpr[0][i], tpr_fpr[1][i]
+                    empirical_eps = analyser.empirical_eps(tpr, fpr, delta=delta)
+                    acc = results.get_accuracy(t)
+                    
+                    if empirical_eps > max_empirical_eps and (empirical_eps != float("inf") or acc == 1):
+                        tp = int((results.scores_train >= t).sum().item())
+                        fp = int((results.scores_test >= t).sum().item())
+                        max_empirical_eps = empirical_eps
+                        max_fp, max_fn, max_tp, max_tn = fp, n_pos-tp, tp, n_neg-fp
+                        max_tpr, max_fpr = tpr, fpr
+            
+            lower_eps = analyser.ci_eps(max_fp, max_fn, n_pos, n_neg, delta=delta)
+            upper_eps = analyser.ci_eps(max_fp, max_fn, bound="upper", n_pos=n_pos, n_neg=n_neg, delta=delta)
+                        
+            extra_cols["fp"].append(max_fp)
+            extra_cols["fn"].append(max_fn)
+            extra_cols["tp"].append(max_tp)
+            extra_cols["tn"].append(max_tn)
             extra_cols["empirical_eps_lower"].append(lower_eps)
             extra_cols["empirical_eps_upper"].append(upper_eps)
-            extra_cols["empirical_eps"].append(empirical_eps)
+            extra_cols["empirical_eps"].append(max_empirical_eps)
 
         for col in extra_cols.keys():
             df[col] = extra_cols[col]
             
         print("Empirical epsilon data added...")
 
-def extract_global_empirical_eps(df, skip_inf=True):
+def extract_global_empirical_eps(df, skip_ci=True):
     df["empirical_global_eps"] = 0
     df["empirical_global_eps_lower"] = 0
     df["empirical_global_eps_upper"] = 0
@@ -161,50 +175,72 @@ def extract_global_empirical_eps(df, skip_inf=True):
         df_eps = temp_df["empirical_eps"].values
         steps = temp_df["global_round"].values
         
-        empirical_global_eps = calculate_global_eps(df_eps, steps=steps, sample_rate=sample_rate, skip_inf=skip_inf)
+        theoretical_sigma = temp_df["final_sigma"].mean()
+
+        empirical_global_eps = calculate_global_eps(df_eps, theoretical_sigma=theoretical_sigma, steps=steps, sample_rate=sample_rate)
         df.loc[df["epsilon"] == eps, 'empirical_global_eps'] = empirical_global_eps
         print(f"eps={eps} estimate done...")
 
-        empirical_global_eps = calculate_global_eps(temp_df["empirical_eps_lower"].clip(lower=0.2).values, steps=steps, sample_rate=sample_rate, skip_inf=skip_inf)
-        df.loc[df["epsilon"] == eps, 'empirical_global_eps_lower'] = empirical_global_eps
-        print(f"eps={eps} lower done...")
+        if not skip_ci:
+            empirical_global_eps = calculate_global_eps(temp_df["empirical_eps_lower"].clip(lower=0.2).values, theoretical_sigma=theoretical_sigma, steps=steps, sample_rate=sample_rate)
+            df.loc[df["epsilon"] == eps, 'empirical_global_eps_lower'] = empirical_global_eps
+            print(f"eps={eps} lower done...")
 
-        empirical_global_eps = calculate_global_eps(temp_df["empirical_eps_upper"].values, steps=steps, sample_rate=sample_rate, skip_inf=skip_inf)
-        df.loc[df["epsilon"] == eps, 'empirical_global_eps_upper'] = empirical_global_eps
-        print(f"eps={eps} upper done...\n")
-        
-    return df
+            empirical_global_eps = calculate_global_eps(temp_df["empirical_eps_upper"].values, theoretical_sigma=theoretical_sigma, steps=steps, sample_rate=sample_rate)
+            df.loc[df["epsilon"] == eps, 'empirical_global_eps_upper'] = empirical_global_eps
+            print(f"eps={eps} upper done...\n")
             
-def calculate_global_eps(empirical_per_step_epsilons, sample_rate=0.01, steps=1000, delta=1e-5, verbose=False, skip_inf=False):
+    return df
+
+def compute_one_step_eps(sample_rate, noise, delta=1e-5):
     accountant = RDPAccountant()
-    previous_step = 0
+    history_step = (noise, sample_rate, 1)
+    accountant.history.append(history_step)
+    current_budget = accountant.get_privacy_spent(delta=delta)
+    return current_budget[0]
+            
+def calculate_global_eps(empirical_per_step_epsilons, theoretical_sigma, sample_rate=0.01, steps=1000, delta=1e-5, n=100, verbose=False):
     if type(steps) == int:
         steps = range(1, steps+1)
+            
+    accountant = RDPAccountant()
+    previous_step = 0
+    if verbose:
+        theoretical_accountant = RDPAccountant()
+        one_step_theoretical_eps = compute_one_step_eps(1, theoretical_sigma, delta)
 
     budgets = []
     for i,step in enumerate(steps):
-        if skip_inf and empirical_per_step_epsilons[i] == float("inf"):
-            if verbose:
-                print("skipped...")
-            budgets.append(float("inf"))
+        # One-step sigma based on current empirical eps
+        if empirical_per_step_epsilons[i] == float("inf"): # Resort to theoretical sigma if empirical eps is inf
+            estimated_sigma = theoretical_sigma
         else:
-            estimated_sigma = get_noise_multiplier(target_epsilon=empirical_per_step_epsilons[i],
-                                                target_delta=delta,
-                                                sample_rate=sample_rate,
-                                                steps=step-previous_step)
-            
-            history_step = (estimated_sigma, sample_rate, step-previous_step)
-            accountant.history.append(history_step)
-            previous_step = step
-            current_budget = accountant.get_privacy_spent(delta=delta)
-            budgets.append(current_budget[0])
-            
-            if verbose:
-                print(f"i={i}, global round={step}")
-                print(f"Estimated empirical eps {empirical_per_step_epsilons[i]}")
-                print("Privacy step:", history_step, "\n")
-                print(f"Accumulated budget {budgets[-1]}")
+            estimated_sigma = get_noise_multiplier(target_epsilon=max(empirical_per_step_epsilons[i], 0.15),
+                                                target_delta=1/n,
+                                                sample_rate=1,
+                                                steps=1)
         
+        # Assume noise is constant for step-previous_step rounds (i.e time between last estimate and current)
+        history_step = (estimated_sigma, sample_rate, step-previous_step)
+        accountant.history.append(history_step)
+        previous_step = step
+        current_budget = accountant.get_privacy_spent(delta=delta)
+        budgets.append(current_budget[0])
+        
+        if verbose:
+            estimated_sigma_theoretical = get_noise_multiplier(target_epsilon=one_step_theoretical_eps,
+                                                target_delta=delta,
+                                                sample_rate=1,
+                                                steps=1)
+            history_step = (estimated_sigma_theoretical, sample_rate, step-previous_step)
+            theoretical_accountant.history.append(history_step)
+            theoretical_eps = theoretical_accountant.get_privacy_spent(delta=delta)[0]
+        
+            print(f"i={i}, global round={step}")
+            print(f"Estimated empirical one-step sigma = {estimated_sigma} vs theoretical = {estimated_sigma_theoretical}")
+            print(f"Estimated empirical one-step epsilon = {empirical_per_step_epsilons[i]} vs theoretical = {one_step_theoretical_eps}")
+            print(f"Accumulated empirical budget {budgets[-1]} vs theoretical {theoretical_eps}\n")
+    
     return budgets
 
 def load_sweep(name, relative_path=False, override_empirical_eps=False):
@@ -300,10 +336,7 @@ if __name__ == "__main__":
         raise FileNotFoundError(f"Output .csv does not exist at the given file path {args.csv_path}")
         
     if not global_eps_csv_file.is_file():
-        print("\nComputing empirical global eps over the rounds... may take a while")
         df = pd.read_csv(args.csv_path)
-        df = df[df["epsilon"]] # TODO: Debugging, remove
-        df = df[df["sweep_id"] == 1] 
         df = extract_global_empirical_eps(df)
         df.to_csv(global_eps_csv_file)
         
